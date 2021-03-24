8ffdbabf0cc4c8e80867fd18a8051e5cc1e40316
TrevorTaoARM <trevor.tao@arm.com>
2021-03-24 17:09:55 +0800

    Add 2 scripts for x86 Snort testing
    Signed-off-by: TrevorTaoARM <trevor.tao@arm.com>

--- run_snort_mthread_x86_numa.sh
+++ run_snort_mthread_x86_numa.sh
@@ -2,6 +2,7 @@
 set -x
 num=$1
 numa=$2
+method=$3
 
 if test $num -ge 160
 then
@@ -16,7 +17,9 @@ fi
 echo "number of insts: $num"
 
 DATE=$(date +"%m-%d-%y-%H-%M")
-PCAP_FILE=$HOME/pcaps/maccdc2012_00001.pcap
+#PCAP_FILE=$HOME/pcaps/maccdc2012_00001.pcap
+PCAP_FILE=$HOME/pcaps/defcon.pcap
+#PCAP_FILE=$HOME/pcaps/get250.pcap
 
 for ((i = 1; i <= $num; i++))
 do
@@ -29,8 +32,23 @@ done
 PCAP_LIST_OPT="--pcap-list=\"${PCAP_LIST}\""
 echo "$PCAP_LIST"
 echo "$PCAP_LIST_OPT"
-HYPERSCAN="search_engine.search_method = \"hyperscan\""
-search_method=$HYPERSCAN
+
+echo "Method: "$method
+if [ $method = "ac_bnfa" ]; then
+	search_method="search_engine.search_method = \"ac_bnfa\""
+elif [ $method = "ac_full" ]; then
+	search_method="search_engine.search_method = \"ac_full\""
+else
+	search_method="search_engine.search_method = \"hyperscan\""
+fi
+
+echo "Search_method: "$search_method
+search_mode=$search_method
+
+
+#HYPERSCAN="search_engine.search_method = \"hyperscan\""
+#search_method=$HYPERSCAN
+
 mkdir -p ./results-numa
 #COMMAND="taskset -c 1-$num snort -z 0  -v --rule-path /usr/local/etc/rules/ -c /usr/local/etc/snort/snort.lua --lua 'search_engine.search_method = "hyperscan"' --pcap-list="$PCAP_LISTi""
 tnum=$[num - 1]
@@ -73,7 +91,13 @@ echo "cpuset:"$cpuset
 		snum=$num
 	fi
 	#CMD="taskset -c 0-$tnum snort -z 0  -v --rule-path /usr/local/etc/rules/ -c /usr/local/etc/snort/snort.lua --lua '$HYPERSCAN' $PCAP_LIST_OPT 2>&1 > ./results/snort-$snum-cores-$search_mode-results-mthread-test.txt"
-	CMD="taskset -c $cpuset snort -z 0  -v --rule-path /usr/local/etc/rules/ -c /usr/local/etc/snort/snort.lua --lua '$HYPERSCAN' $PCAP_LIST_OPT 2>&1 > ./results-numa/snort-$numa-$snum-cores-$search_mode-results-mthread-test.txt"
+	CMD="taskset -c $cpuset snort -z 0  -v --rule-path /usr/local/etc/rules/ -c /usr/local/etc/snort/snort.lua --lua '$search_method' $PCAP_LIST_OPT 2>&1 > ./results-numa/snort-$numa-$snum-cores-$search_mode-results-mthread-test.txt"
+#	if [ $numa = "numa0" ] 
+#	then
+#	CMD="numactl --membind=0 --cpunodebind=0 --physcpubind=$cpuset snort -z 0  -v --rule-path /usr/local/etc/rules/ -c /usr/local/etc/snort/snort.lua --lua '$HYPERSCAN' $PCAP_LIST_OPT 2>&1 > ./results-numa/snort-$numa-$snum-cores-$search_mode-results-mthread-test.txt"
+#	else
+#	CMD="numactl --membind=1 --cpunodebind=1 --physcpubind=$cpuset snort -z 0  -v --rule-path /usr/local/etc/rules/ -c /usr/local/etc/snort/snort.lua --lua '$HYPERSCAN' $PCAP_LIST_OPT 2>&1 > ./results-numa/snort-$numa-$snum-cores-$search_mode-results-mthread-test.txt"
+#	fi
 fi
 
 echo "$CMD"
--- run_snort_mthread_x86_numa_all.sh
+++ run_snort_mthread_x86_numa_all.sh
@@ -4,10 +4,22 @@ num=$1
 
 for i in {1,2,4,8,12,16,20,24}
 do
-	./run_snort_mthread_x86_numa.sh $i numa0
-	./run_snort_mthread_x86_numa.sh $i numa1
+	./run_snort_mthread_x86_numa.sh $i numa0 ac_full
+	./run_snort_mthread_x86_numa.sh $i numa1 ac_full
 done
 
+for i in {1,2,4,8,12,16,20,24}
+do
+	./run_snort_mthread_x86_numa.sh $i numa0 ac_bnfa
+	./run_snort_mthread_x86_numa.sh $i numa1 ac_bnfa
+done
+
+
+for i in {1,2,4,8,12,16,20,24}
+do
+	./run_snort_mthread_x86_numa.sh $i numa0 hyperscan
+	./run_snort_mthread_x86_numa.sh $i numa1 hyperscan
+done
 
 #echo "$CMD"
 #eval $CMD
